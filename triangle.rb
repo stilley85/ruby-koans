d7c631614bdb76b3f9b9744c15cf3c7e18350090
stephen <stephentilley@gmail.com>
2017-12-31 19:13:53 +0000

    added error handling to triangle.rb

--- triangle.rb
+++ triangle.rb
@@ -14,6 +14,15 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
+
+  if a <= 0 || b <= 0 || c <= 0
+    raise TriangleError
+  end
+
+  if (a  +b) <= c || (a + c) <= b || (b + c) <= a
+    raise TriangleError
+  end
+
   if a == b && a == c
     :equilateral
   elsif a == b || a == c || b == c
